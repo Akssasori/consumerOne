97ec850cefb97ef1cc52220f0a3a820b74c0b05f
Akssasori <diniz3003@gmail.com>
2024-10-27 10:56:57 -0300

    ajuste java 21

--- src/main/java/com/lucas/consumerOne/controllers/RabbitController.java
+++ src/main/java/com/lucas/consumerOne/controllers/RabbitController.java
@@ -7,10 +7,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.Base64;
+
 @RestController
 @RequestMapping("rabbit")
 public class RabbitController {
 
+    private static final String RABBITMQ_API_URL = "http://localhost:15672/api/consumers";
+    private static final String USERNAME = "guest";
+    private static final String PASSWORD = "guest";
 
     private final RabbitListenerEndpointRegistry rabbitListenerEndpointRegistry;
 
@@ -32,4 +42,47 @@ public class RabbitController {
         System.out.println("Consumidor com ID " + listenerId + " foi inicido.");
         return ResponseEntity.ok().body("success when start " + listenerId);
     }
+
+    @GetMapping("listerConsumers")
+    public ResponseEntity<String> listerAllConsumers() {
+        String consumers;
+        try {
+            consumers = listerConsumers();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        return ResponseEntity.ok().body(consumers);
+    }
+
+    private String listerConsumers() throws IOException, InterruptedException {
+
+        HttpResponse<String> response = sendRequestToRabbitMq();
+
+        if (response.statusCode() == 200) {
+            System.out.println("consumidores conectados: "+ response.body());
+            return response.body();
+        }
+        else {
+            return "nehum consumidor encontrado";
+        }
+
+    }
+
+    public HttpResponse<String> sendRequestToRabbitMq() throws IOException, InterruptedException {
+        HttpClient client = HttpClient.newHttpClient();
+
+        String auth = USERNAME + ":" + PASSWORD;
+        String encodedAuth = Base64.getEncoder().encodeToString(auth.getBytes());
+
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(RABBITMQ_API_URL))
+                .header("Authorization", "Basic " + encodedAuth)
+                .GET()
+                .build();
+
+//        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+        return client.send(request, HttpResponse.BodyHandlers.ofString());
+    }
+
 }
